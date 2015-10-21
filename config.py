28d909f5d97fee0f65dfed273badd5e0a2a810cd
Valentin Samir <valentin.samir@crans.org>
2015-10-21 19:16:55 +0200

    Add SSHFP + read http headers

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ DOMAINS = ["genua.fr", "samir.re", "martinez.pm"]
 SERVERS = ["10.7.0.1", "10.7.0.2", "10.7.0.4"]
 CERT_BASE_PATH = "/etc/ssl/private/"
 BIND_BASE_PATH = "/etc/bind/generated/db.tlsa."
+SSH_KEYS = "/etc/ssh/"
 PORT = 6204
