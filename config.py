9bd93ed6a4e67cebc5358eb96d8d0843e4b2c4a8
Valentin Samir <valentin.samir@crans.org>
2015-10-21 17:47:01 +0200

    Add a config module + directly write bind files

---
+++ config.py
@@ -0,0 +1,5 @@
+DOMAINS = ["genua.fr", "samir.re", "martinez.pm"]
+SERVERS = ["10.7.0.1", "10.7.0.2", "10.7.0.4"]
+CERT_BASE_PATH = "/etc/ssl/private/"
+BIND_BASE_PATH = "/etc/bind/generated/db.tlsa."
+PORT = 6204
